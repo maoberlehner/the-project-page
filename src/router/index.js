c2ef2c2ee9062cff7e7a5fc6a1628d083dd6cd40
Markus Oberlehner <markus.oberlehner@gmail.com>
2017-05-24 19:14:40 +0200

    Move routes to a separate file to make them reusable

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@/views/Home';
+import routes from '@/routes';
 
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home,
-    },
-  ],
+  routes,
 });
